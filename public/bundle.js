1b20ae8341cf962963a138f080e8da95a62ac468
Martin Aarhus Gregersen <marting12@live.dk>
2016-07-06 22:33:00 +0200

    Finalizes first stage of app

--- public/bundle.js
+++ public/bundle.js
@@ -58,12 +58,18 @@
 
 
 	var Main = __webpack_require__(216);
-	var Nav = __webpack_require__(217);
+	var Weather = __webpack_require__(218);
+	var About = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"About\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
 
 	ReactDOM.render(React.createElement(
 	  Router,
 	  { history: hashHistory },
-	  React.createElement(Route, { path: '/', component: Main })
+	  React.createElement(
+	    Route,
+	    { path: '/', component: Main },
+	    React.createElement(Route, { path: 'about', component: About }),
+	    React.createElement(IndexRoute, { component: Weather })
+	  )
 	), document.getElementById('app'));
 
 /***/ },
@@ -24768,7 +24774,8 @@
 					'h2',
 					null,
 					'Main Component'
-				)
+				),
+				this.props.children
 			);
 		}
 	});
@@ -24796,6 +24803,28 @@
 	});
 
 	module.exports = Nav;
+
+/***/ },
+/* 218 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var React = __webpack_require__(1);
+
+	var Weather = React.createClass({
+		displayName: 'Weather',
+
+		render: function render() {
+			return React.createElement(
+				'h3',
+				null,
+				'Weather Component'
+			);
+		}
+	});
+
+	module.exports = Weather;
 
 /***/ }
 /******/ ]);
