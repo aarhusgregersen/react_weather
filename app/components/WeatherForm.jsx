4072bf1f7948154b071f11839f92619264fc6869
Martin Gregersen JEF <mag@jef.dk>
2016-07-07 14:18:14 +0200

    Add WeatherForm and WeatherMessage

---
+++ app/components/WeatherForm.jsx
@@ -0,0 +1,18 @@
+var react = require('react');
+
+var WeatherForm = React.createClass({ 
+	render: function() {
+		return (
+			<div>
+				<form>
+					<input type="text"/>
+					<button>Get Weather</button>
+				</form>
+			</div>
+
+			);
+	}
+
+});
+
+module.exports = WeatherForm;
