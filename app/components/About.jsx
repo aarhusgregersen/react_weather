1e0b4550d1c8cf9fe1520e12c689d7eb756aa07d
Martin Gregersen JEF <mag@jef.dk>
2016-07-13 11:47:06 +0200

    Updates gitignore and about for ES6 syntax

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,11 +1,19 @@
 var React = require('react');
 
-var About = React.createClass({
-	render: function() {
-		return (
-			<h3>About us</h3>
-		);
-	}
-});
+// var About = React.createClass({
+// 	render: function() {
+// 		return (
+// 			<h3>About us</h3>
+// 		);
+// 	}
+// });
+
+// Stateless functional component (Does the same as above)
+// This can only be done because there's no state
+var About = (props) => {
+	return (
+		<h3>About us</h3>
+	);
+}
 
 module.exports = About;
