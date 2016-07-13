fde78bd00f7631167801519092930f7b05b6f9bc
Martin Gregersen JEF <mag@jef.dk>
2016-07-13 11:56:23 +0200

    Further cleans up functions and utilizes ES6 standards

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,14 +1,6 @@
 var React = require('react');
 
-// var Examples = React.createClass({
-// 	render: function() {
-// 		return (
-// 			<h3>Examples shown here</h3>
-// 		);
-// 	}
-// });
-
-// Stateless functional component (Does the same as above)
+// Stateless functional component
 // This can only be done because there's no state and the only content is return function
 var Examples = (props) => {
 	return (
