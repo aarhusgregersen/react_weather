4d99df1629a32808ba70d371c0cab8040e40d276
Martin Aarhus Gregersen <marting12@live.dk>
2016-07-13 21:11:42 +0200

    Re-initalizes app on git

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 // Express Middleware: Let's us do something with each request
-app.use(function (req, res, next) {
-	if (req.headers['x-forwarded-proto'] === 'http') {
-		next();
-	} else  {
-		res.redirect('http://' + req.hostname + req.url);
-	}
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
 });
 
 app.use(express.static('public'));
