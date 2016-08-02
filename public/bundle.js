06431eeb02e7cf4ae2937c1cf52d04f9c99f48b6
Martin Aarhus Gregersen <marting12@live.dk>
2016-08-02 19:52:33 +0200

    Updates navbar with style

--- public/bundle.js
+++ public/bundle.js
@@ -24853,34 +24853,109 @@
 	var IndexLink = _require.IndexLink;
 
 
-	var Nav = function Nav(props) {
-		return React.createElement(
-			'div',
-			null,
-			React.createElement(
-				'h2',
-				null,
-				'Nav Component'
-			),
-			React.createElement(
-				IndexLink,
-				{ to: '/', activeClassName: 'active', activeStyle: { fontWeight: 'bold' } },
-				'Get Weather'
-			),
-			React.createElement(
-				IndexLink,
-				{ to: '/about', activeClassName: 'active', activeStyle: { fontWeight: 'bold' } },
-				'About'
-			),
-			React.createElement(
-				IndexLink,
-				{ to: '/examples', activeClassName: 'active', activeStyle: { fontWeight: 'bold' } },
-				'Examples'
-			)
-		);
-	};
+	var Nav = React.createClass({
+	  displayName: 'Nav',
+
+	  onSearch: function onSearch(e) {
+	    e.preventDefault();
+	    alert("Not yet wired up.");
+	  },
+	  render: function render() {
+	    return React.createElement(
+	      'div',
+	      { className: 'top-bar' },
+	      React.createElement(
+	        'div',
+	        { className: 'top-bar-left' },
+	        React.createElement(
+	          'ul',
+	          { className: 'menu' },
+	          React.createElement(
+	            'li',
+	            { className: 'menu-text' },
+	            'Gregersens Vejr-app i React'
+	          ),
+	          React.createElement(
+	            'li',
+	            null,
+	            React.createElement(
+	              IndexLink,
+	              { to: '/', activeClassName: 'active', activeStyle: { fontWeight: 'bold' } },
+	              'Get Weather'
+	            )
+	          ),
+	          React.createElement(
+	            'li',
+	            null,
+	            React.createElement(
+	              IndexLink,
+	              { to: '/about', activeClassName: 'active', activeStyle: { fontWeight: 'bold' } },
+	              'About'
+	            )
+	          ),
+	          React.createElement(
+	            'li',
+	            null,
+	            React.createElement(
+	              IndexLink,
+	              { to: '/examples', activeClassName: 'active', activeStyle: { fontWeight: 'bold' } },
+	              'Examples'
+	            )
+	          )
+	        )
+	      ),
+	      React.createElement(
+	        'div',
+	        { className: 'top-bar-right' },
+	        React.createElement(
+	          'form',
+	          { onSubmit: this.onSearch },
+	          React.createElement(
+	            'ul',
+	            { className: 'menu' },
+	            React.createElement(
+	              'li',
+	              null,
+	              React.createElement('input', { type: 'search', placeholder: 'Search weather' })
+	            ),
+	            React.createElement(
+	              'li',
+	              null,
+	              React.createElement('input', { type: 'submit', className: 'button', value: 'Get Weather' })
+	            )
+	          )
+	        )
+	      )
+	    );
+	  }
+	});
 
 	module.exports = Nav;
+
+	var old = React.createElement(
+	  'div',
+	  null,
+	  React.createElement(
+	    'h2',
+	    null,
+	    'Nav Component'
+	  ),
+	  React.createElement(
+	    IndexLink,
+	    { to: '/', activeClassName: 'active', activeStyle: { fontWeight: 'bold' } },
+	    'Get Weather'
+	  ),
+	  React.createElement(
+	    IndexLink,
+	    { to: '/about', activeClassName: 'active', activeStyle: { fontWeight: 'bold' } },
+	    'About'
+	  ),
+	  React.createElement(
+	    IndexLink,
+	    { to: '/examples', activeClassName: 'active', activeStyle: { fontWeight: 'bold' } },
+	    'Examples'
+	  )
+	);
 
 /***/ },
 /* 225 */
