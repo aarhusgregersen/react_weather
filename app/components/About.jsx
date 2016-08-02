2affaa359b0b25cbbde41e5b8628c2ad8f5aa8cb
Martin Aarhus Gregersen <marting12@live.dk>
2016-08-02 18:58:11 +0200

    Updates content for about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,9 +3,12 @@ var React = require('react');
 // Stateless functional component
 // This can only be done because there's no state and the only content is return function
 var About = (props) => {
-	return (
-		<h3>About us</h3>
-	)
+  return (
+    <div>
+      <h3>About</h3>
+      <p>Welcome to the about page</p>
+    </div>
+  )
 };
 
 module.exports = About;
